c14ef7e96af0f4c90a041c8ce7b80e3457811dfe
Nicholas <NickyBobby@users.noreply.github.com>
2019-02-10 01:41:34 -0600

    Removes unnecessary :level for node class smh....

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,5 @@
 class Node
-  attr_accessor :left_link, :right_link, :level
+  attr_accessor :left_link, :right_link
   attr_reader :data
 
   def initialize(data, left_link = nil, right_link = nil)
